adb2c2738c47554032fd5bab633edfcf0422eb87
om bichukale <bichukaleom2003@gmail.com>
2025-06-17 12:35:56 +0530

    Implement Sign_in Page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,8 +1,15 @@
+import Companionaform from "@/components/Companionaform";
 
 
 const NewCompanion = () => {
   return (
-    <div>NewCompanion</div>
+    <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
+      <article className="w-full gap-4 flex flex-col">
+        <h1>Companion Builder</h1>
+         <Companionaform/>
+      </article>
+
+    </main>
   );
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,55 @@
-import React from 'react'
-import {Button} from "@/components/ui/button";
+
+import CompanionCard from "@/components/CompanionCard";
+import CTA from "@/components/CTA";
+import CompanionList from "@/components/CompanionList";
+import {recentSessions} from "@/constants";
+
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my saas app </h1>
-      <Button>
-        Lets get started
-      </Button>
-    </div>
+    <main>
+      <h1 >Popular Companions</h1>
+     <section className="home-section">
+         <CompanionCard
+         id="123"
+         name="Neura the Brainy Explorer"
+         topic="Neural Network of the Brain"
+         subject="science"
+         duration={45}
+         color="#ffda6e"
+         />
+
+         <CompanionCard
+             id="456"
+             name="Countsy the Number Wizard"
+             topic="Derivatives & Integrals"
+             subject="Maths"
+             duration={30}
+             color="#e5d0ff"
+         />
+
+         <CompanionCard
+             id="789"
+             name="Verba the Vocabulary Builder"
+             topic="language"
+             subject="English Literature"
+             duration={30}
+             color="#BDE7FF"
+         />
+
+
+     </section>
+
+        <section className="home-section">
+            <CompanionList
+            title="Recently completed sessions"
+            companions={recentSessions}
+            classNames="w-2/3 max-lg:w-full"
+            />
+
+            <CTA />
+        </section>
+    </main>
   )
 }
 
